9a80fb7a6646926b12b4bdab12e9a1e0daa5cb22
toshiro <toshiro@localhost.localdomain>
2014-02-09 22:40:45 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GroupTalk::Application.config.secret_key_base = '9ceff4c4917dde647028e242e9c0c9ce71e289673c041440a09557c1764cd69a26b2be3d37d411a2b148de9698192993744729b2cc8e2e8b69944f6fa48d0122'
